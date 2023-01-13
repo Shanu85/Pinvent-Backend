31f04ffbe78ebe5e76d25f8d066da72e1db2143c
Shanu <63155782+Shanu85@users.noreply.github.com>
2023-01-13 21:43:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:false}))
 app.use(bodyparser.json())
 app.use(cookieParser());
 app.use(cors({
-    origin:['http://localhost:3000','https://pinvent-app.vercel.app'],
+    origin:['http://localhost:3000','https://pinvent-app-bice.vercel.app/'],
     credentials:true
 }));
 app.use(upload.array());
